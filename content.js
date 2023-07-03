f926a3854e0169caa4fc88f1406df3e1089bd2b0
phantasyhandle <138405541+phantasyhandle@users.noreply.github.com>
2023-07-03 12:19:52 -0600

    Update content.js

--- content.js
+++ content.js
@@ -1,14 +1,12 @@
+document.addEventListener('DOMNodeInserted', removeShitHeadsAndShitThreads);
 
-setInterval(removeShitThreads, 1000);
-
-setInterval(removeShitHeads, 1000);
-
-function removeShitThreads() {
+function removeShitHeadsAndShitThreads() {
     chrome.storage.sync.get(
-        { threadsToIgnore: '', postToIgnore: '' },
+        { shitHeadsAndShitThreadsToIgnore: ''},
         (items) => {
-            const blockedWords = items.threadsToIgnore.split(',').map(s => s.toLowerCase().trim());
+            const blockedWords = items.shitHeadsAndShitThreadsToIgnore.split(',').map(s => s.toLowerCase().trim());
             if(blockedWords.length>0 && blockedWords[0]){
+                //looks for shit words in thread title
                 var listOfShitThreads = Array.from(document.getElementsByClassName("hide_overflow"));
                 for (let item of listOfShitThreads) {
                     const text = (item.textContent || '').toLowerCase();
@@ -18,22 +16,34 @@ function removeShitThreads() {
                             item.parentElement.style['display'] = 'none';
                         }
                     }
-                    
                 }
-            }
-            
-        }
-    );
-}
 
-function removeShitHeads() {
-    chrome.storage.sync.get(
-        { threadsToIgnore: '', postToIgnore: '' },
-        (items) => {
-            const blockedWords = items.postToIgnore.split(',').map(s => s.toLowerCase().trim());
-            if(blockedWords.length>0 && blockedWords[0]){
-                var listOfShitHeads = Array.from(document.getElementsByClassName("post_body_container"));
-                for (let item of listOfShitHeads) {
+                //looks for shithead author of thread
+                listOfShitThreads = Array.from(document.getElementsByClassName("topic_author_display_name"));
+                for (let item of listOfShitThreads) {
+                    const text = (item.textContent || '').toLowerCase();
+                    if (text.length){
+                        var stringIncludesShit = blockedWords.some(shit => text.includes(shit));
+                        if (stringIncludesShit) {
+                            item.parentElement.style['display'] = 'none';
+                        }
+                    }
+                }
+                //removes shit words in post
+                var listOfShitPosts = Array.from(document.getElementsByClassName("post_body_container"));
+                for (let item of listOfShitPosts) {
+                    const text = (item.textContent || '').toLowerCase();
+                    if (text.length){
+                        var stringIncludesShit = blockedWords.some(shit => text.includes(shit));
+                        if (stringIncludesShit) {
+                            item.parentElement.parentElement.style['display'] = 'none';
+                        }
+                    }
+                }
+                
+                //removes posts by shithead author
+                var listOfShitPosts = Array.from(document.getElementsByClassName("poster_name"));
+                for (let item of listOfShitPosts) {
                     const text = (item.textContent || '').toLowerCase();
                     if (text.length){
                         var stringIncludesShit = blockedWords.some(shit => text.includes(shit));
